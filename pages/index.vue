7547688aa47e2e6d6fe9c84a409143591486e933
madebyfabian <hello@madebyfabian.com>
2023-02-12 17:43:14 +0100

    feat: add demo pages

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Welcome to supastarter!</h1>
+    <NuxtLink to="/protected">
+      <span>Visit a Protected page &rarr;</span>
+    </NuxtLink>
+    <br><br>
+    <pre>tRPC Data: "{{ hello.data.value?.greeting }}" send at "{{ hello.data.value?.time.toLocaleDateString('en-EN') }}".</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { $trpcClient } = useNuxtApp()
+const hello = await $trpcClient.hello.useQuery({ text: 'client' })
+</script>
---
+++ pages/protected.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>I'm protected! Session data: {{ data }}</div>
+    <NuxtLink to="/">
+      <span>&larr; Back Home</span>
+    </NuxtLink>
+    <button class="rounded-xl shadow-xl p-2 m-2" @click="signOut()">
+      sign out
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({ middleware: 'auth' })
+
+const { data, signOut } = useSession()
+</script>
