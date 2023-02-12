a7f3a4b13f14b39f1744b3a6cdebd1f698a76609
madebyfabian <hello@madebyfabian.com>
2023-02-12 18:05:56 +0100

    fix: added test pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1>Welcome to supastarter!</h1>
-    <NuxtLink to="/protected">
-      <span>Visit a Protected page &rarr;</span>
+    <NuxtLink to="/app/dashboard">
+      <span>Visit Dashboard &rarr;</span>
     </NuxtLink>
     <br><br>
     <pre>tRPC Data: "{{ hello.data.value?.greeting }}" send at "{{ hello.data.value?.time.toLocaleDateString('en-EN') }}".</pre>
