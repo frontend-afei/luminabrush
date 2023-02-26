0fe85e2645a26afa4e8b4675944eb659d325a976
madebyfabian <hello@madebyfabian.com>
2023-02-26 01:33:05 +0100

    fix: nuxt instance not available

--- modules/common/composables/useTrpcClient.ts
+++ modules/common/composables/useTrpcClient.ts
@@ -2,15 +2,15 @@ import superjson from 'superjson'
 import { createTRPCNuxtClient, httpBatchLink } from 'trpc-nuxt/client'
 import type { AppRouter } from '@/server/trpc/routers'
 
-const trpcClient = createTRPCNuxtClient<AppRouter>({
-  transformer: superjson,
-  links: [
-    httpBatchLink({
-      url: '/api/trpc'
-    })
-  ]
-})
-
 export const useTrpcClient = () => {
+  const trpcClient = createTRPCNuxtClient<AppRouter>({
+    transformer: superjson,
+    links: [
+      httpBatchLink({
+        url: '/api/trpc'
+      })
+    ]
+  })
+
   return { trpcClient }
 }
