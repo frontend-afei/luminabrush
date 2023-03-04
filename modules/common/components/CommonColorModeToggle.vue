cd0f833afff134c2686d81553549fb2a25b4e988
madebyfabian <hello@madebyfabian.com>
2023-03-04 17:00:19 +0100

    feat: add zag, added `CommonMenu` and `CommonMenuItem` and added new `CommonColorModeToggle`

--- modules/common/components/CommonColorModeToggle.vue
+++ modules/common/components/CommonColorModeToggle.vue
@@ -1,20 +1,58 @@
 <template>
-  <select
-    v-model="colorMode.preference"
-    class="CommonThemeSwitch text-right bg-transparent font-bold cursor-pointer"
-  >
-    <option value="system">
-      🖥️&nbsp;&nbsp;{{ $t('theme.system') }}
-    </option>
-    <option value="light">
-      💡&nbsp;&nbsp;{{ $t('theme.light') }}
-    </option>
-    <option value="dark">
-      🌙&nbsp;&nbsp;{{ $t('theme.dark') }}
-    </option>
-  </select>
+  <CommonButton v-bind="(api.triggerProps as any)" intent="primary-outline" size="small">
+    <Icon :name="colorMode.value === 'dark' ? 'feather:moon' : 'feather:sun'" />
+  </CommonButton>
+
+  <div v-bind="api.positionerProps">
+    <CommonMenu v-bind="api.contentProps">
+      <CommonMenuItem
+        v-for="option in colorModeOptions"
+        :key="option.value"
+        :icon="option.icon"
+        v-bind="api.getOptionItemProps({ name: 'colorMode', type: 'radio', value: option.value })"
+      >
+        {{ option.label }}
+      </CommonMenuItem>
+    </CommonMenu>
+  </div>
 </template>
 
 <script setup lang="ts">
+import * as menu from '@zag-js/menu'
+import { normalizeProps, useMachine } from '@zag-js/vue'
+
+const { t } = useI18n()
 const colorMode = useColorMode()
+
+const [state, send] = useMachine(menu.machine({
+  id: 'color-mode-toggle',
+  'aria-label': t('common.colorMode.label'),
+  value: { colorMode: colorMode.value },
+  onValueChange: ({ value }) => {
+    if (Array.isArray(value)) {
+      value = value[0]
+    }
+    colorMode.preference = value
+  },
+  loop: true
+}))
+const api = computed(() => menu.connect(state.value, send, normalizeProps))
+
+const colorModeOptions = computed(() => ([
+  {
+    value: 'system',
+    label: t('common.colorMode.system'),
+    icon: 'feather:hard-drive'
+  },
+  {
+    value: 'light',
+    label: t('common.colorMode.light'),
+    icon: 'feather:sun'
+  },
+  {
+    value: 'dark',
+    label: t('common.colorMode.dark'),
+    icon: 'feather:moon'
+  }
+]))
 </script>
---
+++ modules/common/components/CommonMenuItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li
+    class="flex cursor-pointer items-center justify-start gap-3 rounded-md px-4 py-1.5 not-italic data-[focus]:bg-zinc-100 data-[checked]:font-bold data-[checked]:text-zinc-900 dark:data-[focus]:bg-zinc-800 dark:data-[checked]:text-white"
+  >
+    <Icon v-if="props.icon" :name="props.icon" />
+    <span><slot /></span>
+  </li>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  icon?: string
+}>()
+</script>
