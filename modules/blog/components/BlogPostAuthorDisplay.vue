98819bf08bc5f181c3357b1c32cd7a23641ac286
madebyfabian <hello@madebyfabian.com>
2023-03-04 18:28:11 +0100

    fix: made parts reusable

---
+++ modules/blog/components/BlogPostAuthorDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="props.post.authorName" class="flex items-center">
+    <div v-if="props.post.authorImage" class="relative mr-2 h-8 w-8 overflow-hidden rounded-full">
+      <img :src="props.post.authorImage" :alt="props.post.authorName" class="h-full w-full object-cover object-center">
+    </div>
+
+    <div>
+      <p class="text-sm font-semibold opacity-50">
+        {{ props.post.authorName }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { BlogPost } from '@/modules/blog/types'
+const props = defineProps<{
+  post: BlogPost
+}>()
+</script>
--- modules/blog/components/BlogPostListItem.vue
+++ modules/blog/components/BlogPostListItem.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="rounded-xl border p-6 dark:border-zinc-800">
-    <div v-if="post.tags" class="mb-2 flex flex-wrap gap-2">
-      <span v-for="tag of post.tags" :key="tag" class="text-xs font-semibold uppercase tracking-wider text-primary-500">
-        #{{ tag }}
-      </span>
-    </div>
+    <BlogPostTagListDisplay :post="post" />
 
     <NuxtLink :to="localePath({ path: post._path })" class="text-xl font-semibold text-zinc-900 dark:text-white">
       <h2>{{ post.title }}</h2>
@@ -15,17 +11,7 @@
     </p>
 
     <div class="mt-4 flex items-center justify-between">
-      <div v-if="post.authorName" class="flex items-center">
-        <div v-if="post.authorImage" class="relative mr-2 h-8 w-8 overflow-hidden rounded-full">
-          <img :src="post.authorImage" class="h-full w-full object-cover object-center">
-        </div>
-
-        <div>
-          <p class="text-sm font-semibold opacity-50">
-            {{ post.authorName }}
-          </p>
-        </div>
-      </div>
+      <BlogPostAuthorDisplay :post="post" class="mb-2" />
 
       <div class="ml-auto mr-0">
         <p class="text-sm opacity-30">
