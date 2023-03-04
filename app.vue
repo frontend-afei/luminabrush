e5b0acdd33e45477403aedcfebdf3a8d3b986a2d
madebyfabian <hello@madebyfabian.com>
2023-03-04 13:31:53 +0100

    fix: resolve type issue

--- app.vue
+++ app.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <ColorScheme placeholder="" tag="div">
+    <ColorScheme :name="undefined" :props="colorSchemeProps" class="haha">
       <NuxtLayout>
         <NuxtPage />
       </NuxtLayout>
     </ColorScheme>
   </div>
 </template>
+
+<script lang="ts" setup>
+/** Workaround for https://github.com/nuxt-modules/color-mode/issues/168 */
+const colorSchemeProps = ref<any>(undefined)
+</script>
