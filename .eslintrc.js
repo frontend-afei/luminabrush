0e821a004ce356317432207305fd760337db588d
madebyfabian <hello@madebyfabian.com>
2023-10-14 17:53:45 +0200

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
 	root: true,
 	// This tells ESLint to load the config from the package `eslint-config-custom`
 	extends: ['eslint-config-custom'],
-	noTabs: 0,
+	rules: {
+		'no-tabs': 0,
+	},
+	indent: ['error', 'tab'],
 }
