04ed7086d4222a485f2c75e396458b8cf3fd8705
madebyfabian <hello@madebyfabian.com>
2023-10-19 17:06:50 +0200

    feat: add card

---
+++ apps/web/modules/ui/components/Card.vue
@@ -0,0 +1,13 @@
+<template>
+	<div :class="cn('bg-card text-card-foreground rounded-lg shadow', props.class)">
+		<slot />
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { cn, type ClassValue } from '@/modules/ui/lib/utils'
+
+	const props = defineProps<{
+		class?: ClassValue
+	}>()
+</script>
---
+++ apps/web/modules/ui/components/Card/CardHeader.vue
@@ -0,0 +1,17 @@
+<template>
+	<div :class="cn('flex flex-col space-y-1.5 p-6', props.class)">
+		<slot />
+
+		<h3 v-if="$slots.title" class="text-xl font-semibold leading-none">
+			<slot name="title" />
+		</h3>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { cn, type ClassValue } from '@/modules/ui/lib/utils'
+
+	const props = defineProps<{
+		class?: ClassValue
+	}>()
+</script>
