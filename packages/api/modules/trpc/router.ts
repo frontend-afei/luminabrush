8f3d0ce5a17b6ff3965253a4c3b904cdb073cf78
madebyfabian <hello@madebyfabian.com>
2023-10-30 23:31:05 +0100

    feat: added ai demo

--- packages/api/modules/trpc/router.ts
+++ packages/api/modules/trpc/router.ts
@@ -1,21 +1,17 @@
 import { inferRouterInputs, inferRouterOutputs } from '@trpc/server'
-import * as newsletterProcedures from '../newsletter/procedures'
 import * as authProcedures from '../auth/procedures'
 import * as billingProcedures from '../billing/procedures'
 import * as teamProcedures from '../team/procedures'
+import * as newsletterProcedures from '../newsletter/procedures'
+import * as aiProcedures from '../ai/procedures'
 import { router } from './trpc'
 
 export const apiRouter = router({
-	newsletter: router(newsletterProcedures),
 	auth: router(authProcedures),
 	billing: router(billingProcedures),
 	team: router(teamProcedures),
-	/**
-	 * @TODO
-	 */
-	/*
-	ai:
-	*/
+	newsletter: router(newsletterProcedures),
+	ai: router(aiProcedures),
 })
 
 export type ApiRouter = typeof apiRouter
