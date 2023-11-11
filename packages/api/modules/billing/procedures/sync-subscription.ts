87b205c265f4f45e4f2c641fa07af7558118c891
madebyfabian <hello@madebyfabian.com>
2023-11-11 11:05:40 +0100

    fix: align with supastarter-nextjs `9649be9`

--- packages/api/modules/billing/procedures/sync-subscription.ts
+++ packages/api/modules/billing/procedures/sync-subscription.ts
@@ -13,10 +13,10 @@ export const syncSubscription = publicProcedure
 
 		let existingSubscription: Subscription | null = null
 
-		if (subscription?.id) {
+		if (subscription?.team_id) {
 			existingSubscription = await db.subscription.findFirst({
 				where: {
-					id: subscription.id,
+					team_id: subscription.team_id,
 				},
 			})
 		}
@@ -29,7 +29,7 @@ export const syncSubscription = publicProcedure
 			else
 				await db.subscription.update({
 					where: {
-						id: existingSubscription.id,
+						team_id: existingSubscription.team_id,
 					},
 					data: subscription,
 				})
