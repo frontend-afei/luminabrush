613854d95093d150a119214bd8af3ba9828b6a87
madebyfabian <hello@madebyfabian.com>
2023-11-12 11:58:46 +0100

    refactor: make eslint happy

--- packages/mail/provider/plunk.ts
+++ packages/mail/provider/plunk.ts
@@ -1,4 +1,4 @@
-import { SendEmailHandler } from '../types'
+import type { SendEmailHandler } from '../types'
 
 export const send: SendEmailHandler = async ({ to, subject, html, text }) => {
 	const response = await fetch('https://api.useplunk.com/v1/send', {
--- packages/mail/provider/resend.ts
+++ packages/mail/provider/resend.ts
@@ -1,9 +1,9 @@
 import { config } from '../config'
-import { SendEmailHandler } from '../types'
+import type { SendEmailHandler } from '../types'
 
 const { from } = config
 
-export const send: SendEmailHandler = async ({ to, subject, html, text }) => {
+export const send: SendEmailHandler = async ({ to, subject, html }) => {
 	const response = await fetch('https://api.resend.com/emails', {
 		method: 'POST',
 		headers: {
