43ec5e3a18cdca9fd1db83606986c77b24cce4e0
madebyfabian <hello@madebyfabian.com>
2023-11-14 13:39:51 +0100

    fix: used named imports to remove console warning of unused imports

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,3 +1,12 @@
-export * from "@prisma/client";
-export * from "./src/client";
-export * from "./src/zod";
+// export * from '@prisma/client' will lead to a warning in the console.
+export {
+	UserRole,
+	UserOneTimePasswordType,
+	TeamMemberRole,
+	SubscriptionStatus,
+	type Team,
+	type TeamMembership,
+	type Subscription,
+} from '@prisma/client'
+export * from './src/client'
+export * from './src/zod'
