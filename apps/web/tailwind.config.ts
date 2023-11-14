fca1297d3926a39af5468075ccae3c89eaaaca97
madebyfabian <hello@madebyfabian.com>
2023-11-14 13:33:10 +0100

    fix: verbatim module syntax

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,5 +1,5 @@
 import tailwindConfig from 'tailwind-config'
-import { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
 	presets: [tailwindConfig],
