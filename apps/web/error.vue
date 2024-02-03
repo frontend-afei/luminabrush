d81b85f581eb14ecd1c220cc96d2cf8c55cef01d
madebyfabian <hello@madebyfabian.com>
2024-02-03 16:14:58 +0100

    fix: align with supastarter-nextjs@e7f0770

---
+++ apps/web/error.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="flex h-screen flex-col items-center justify-center">
+		<h1 class="text-5xl font-bold">404</h1>
+		<p class="mt-2 text-2xl">Page not found</p>
+
+		<Button asChild class="mt-4">
+			<NuxtLink href="/"> <Icon name="undo" class="mr-2 h-4 w-4" /> Go to homepage </NuxtLink>
+		</Button>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
