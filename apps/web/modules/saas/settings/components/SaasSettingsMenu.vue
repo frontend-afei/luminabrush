7c7141242e47053b01316d88d0bac80bd648fa39
Jonathan Wilke <info@jonathan-wilke.de>
2024-03-10 17:29:52 +0100

    fix: load avatar urls for team avatar

--- apps/web/modules/saas/settings/components/SaasSettingsMenu.vue
+++ apps/web/modules/saas/settings/components/SaasSettingsMenu.vue
@@ -2,7 +2,11 @@
 	<div class="space-y-8" v-if="activeTeam && user">
 		<SaasSettingsMenuGroup v-for="(item, key) of props.menuItems" :menuItemGroup="item" :key="key">
 			<template #avatar>
-				<TeamAvatar v-if="item.avatar === 'team'" class="size-8" :name="activeTeam.name" />
+				<TeamAvatar
+					v-if="item.avatar === 'team'"
+					class="size-8"
+					:name="activeTeam.name"
+					:avatar-url="activeTeam.avatarUrl" />
 				<UserAvatar v-else-if="item.avatar === 'user'" :name="user.name || ''" :avatarUrl="user.avatarUrl" />
 			</template>
 		</SaasSettingsMenuGroup>
