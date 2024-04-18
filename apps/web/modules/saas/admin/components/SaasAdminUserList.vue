c87a5c3c528bd79201eace61a4b972fc9c10476c
Jonathan Wilke <info@jonathan-wilke.de>
2024-04-18 08:13:13 +0200

    fix: pagination for user list

--- apps/web/modules/saas/admin/components/SaasAdminUserList.vue
+++ apps/web/modules/saas/admin/components/SaasAdminUserList.vue
@@ -36,7 +36,7 @@
     },
   );
 
-  watch(data, () => {
+  watch(throttledSearchTeam, () => {
     currentPage.value = 1;
   });
 
@@ -124,12 +124,40 @@
     </div>
 
     <Pagination
-      v-if="users?.length > 0"
       class="mt-4"
-      :totalItems="data?.total ?? 0"
-      :itemsPerPage="itemsPerPage"
-      :currentPage="currentPage"
-      onChangeCurrentPage="setCurrentPage"
-    />
+      v-slot="{ page }"
+      :page="currentPage"
+      @update:page="(page) => (currentPage = page)"
+      :total="data?.total ?? 0"
+      :sibling-count="1"
+      show-edges
+      :default-page="1"
+    >
+      <PaginationList
+        v-slot="{ items }"
+        class="flex items-center gap-1 justify-center"
+      >
+        <PaginationPrev />
+
+        <template v-for="(item, index) in items">
+          <PaginationListItem
+            v-if="item.type === 'page'"
+            :key="index"
+            :value="item.value"
+            as-child
+          >
+            <Button
+              class="w-10 h-10 p-0"
+              :variant="item.value === page ? 'default' : 'outline'"
+            >
+              {{ item.value }}
+            </Button>
+          </PaginationListItem>
+          <PaginationEllipsis v-else :key="item.type" :index="index" />
+        </template>
+
+        <PaginationNext />
+      </PaginationList>
+    </Pagination>
   </div>
 </template>
