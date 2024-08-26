31c2083d3210149c7876502511e3e1dd9ab48285
timo2k <timo.ischen@gmail.com>
2024-08-26 21:21:05 +0200

    chore: remove todo comments

--- packages/i18n/lib/messages.ts
+++ packages/i18n/lib/messages.ts
@@ -11,12 +11,10 @@ export const getMessagesForLocale = async (
 ): Promise<Messages> => {
   const localeMessages = await importLocale(locale);
 
-  // TODO: Add config for this property
   if (locale === config.i18n.defaultLocale) {
     return localeMessages;
   }
 
-  // TODO: Add config for this property
   const defaultLocaleMessages = await importLocale(config.i18n.defaultLocale);
   return deepmerge(defaultLocaleMessages, localeMessages);
 };
